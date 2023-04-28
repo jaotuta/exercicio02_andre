7eaf9e06320bdf2b77e12798f98bc6386c758536
Joao Lucas Cruz <jaotuta@hotmail.com>
2023-04-27 21:08:00 -0300

    exercicio de logger

--- DebugExemplo/DebugExemplo/src/Calculadora.java
+++ DebugExemplo/DebugExemplo/src/Calculadora.java
@@ -1,24 +1,26 @@
 
 public class Calculadora {
-	public float calcular(float a, float b, char operacao) {
+
+	public float calcular(float a, float b, char operacao, Log log) {
+		log.logger.info("Dentro da classe Calculadora, método calcular.");
 		float resultado = 0;
 		switch (operacao) {
 		case '+': {
 			resultado = a + b;
-
+			log.logger.info("O case '+' foi executado");
 		}
 		case '-': {
-			resultado = a - b;
-
+			resultado = ( a > b )  ? a - b : b - a;
+			log.logger.info("O case '-' foi executado");
 		}
 		case '/': {
 			resultado = a / b;
+			log.logger.info("O case '/' foi executado");
 		}
 		case '*': {
 			resultado = a * b;
-
+			log.logger.info("O case '*' foi executado");
 		}
-
 		}
 		return resultado;
 
--- DebugExemplo/DebugExemplo/src/Principal.java
+++ DebugExemplo/DebugExemplo/src/Principal.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
-
-	public static void main(String[] args) {
+	public Principal() throws IOException {
+	}
+	public static void main(String[] args) throws IOException {
+		Log log = new Log("Log.txt");
 		try (Scanner leitura = new Scanner(System.in)) {
+			log.logger.info("A Aplicação calculadora foi iniciada.");
 			Calculadora c = new Calculadora();
 			float a = 0, b = 0, resultado = 0;
 			String operador;
@@ -18,9 +22,10 @@ public class Principal {
 			a = leitura.nextFloat();
 			System.out.println("Insira o 2o valor");
 			b = leitura.nextFloat();
-
-			resultado = c.calcular(a, b, operador.charAt(0));
+			log.logger.info("Informações para as operações foram coletadas, operador: " + operador + "a: "+ a + "b: " + b);
+			resultado = c.calcular(a, b, operador.charAt(0), log);
 			System.out.println("O resultado de " + a + " " + operador + " " + b + " é:" + resultado);
+			log.logger.info("Operações da aplicação foram finalizadas com o resultado: " + resultado);
 		}
 
 	}
